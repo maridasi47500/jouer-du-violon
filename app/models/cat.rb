844631979a7647d9ebe6a80af8ae306da0c48f61
CJeanne ANonym <anonyme201504@gmail.com>
2023-09-15 11:10:43 -0300

    jdv

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,4 +3,7 @@ class Cat < ApplicationRecord
   def self.findmycat(x)
     where("parameterize(name) like ?", x)[0]
   end
+  def self.partition
+    where("name like ?", "%partition%")[0]
+  end
 end
